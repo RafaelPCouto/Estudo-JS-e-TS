f2f12195eb4b4e90199cb6d0deb4fa9c7cbfee3a
Rafael Couto <rafaelcouto58@gmail.com>
2024-08-22 16:37:36 -0300

    Documentando e revisando operadores ternários

---
+++ Ternários/verificarMaioridade.js
@@ -0,0 +1,19 @@
+let mensagem;
+
+/*
+if (idade >= 18) {
+    mensagem = 'Você é maior de idade';
+} else {
+    mensagem = 'Você é menor de idade';
+}
+
+console.log(mensagem);
+*/
+
+function verificarMaioridade(idade) {
+    return idade >= 18 ? mensagem = 'Você é maior de idade' : mensagem = 'Você é menor de idade';
+};
+
+console.log(verificarMaioridade(18));
+
+console.log(verificarMaioridade(17));
